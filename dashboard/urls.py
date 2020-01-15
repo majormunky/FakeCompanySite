e107860c590d306c3d60835f458fcf05302a81aa
Josh Bright <josh.bright@gmail.com>
2020-01-14 20:10:45 -0800

    Setup the dashboard orders page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from dashboard import views
 
 urlpatterns = [
 	path('', views.index),
+	path('orders/', views.orders),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, "dashboard/index.html", {})
+
+
+def orders(request):
+	return render(request, "dashboard/orders.html", {})
