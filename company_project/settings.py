adc7e4ade6d3620fca89c591568b9bbe58e44070
Josh Bright <josh.bright@gmail.com>
2020-01-16 17:56:00 -0800

    Setup static serving to be different based on if we are in debug or not.  Will look into later

--- company_project/settings.py
+++ company_project/settings.py
@@ -109,7 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 AUTH_USER_MODEL = "core.CoreUser"
 
